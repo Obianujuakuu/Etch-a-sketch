bed9171b8b5fdadff3c1ab0e3d7ec702a731d193
Obianujuakuu <99666909+Obianujuakuu@users.noreply.github.com>
2023-01-23 03:20:12 +0000

    Added  div and button to index.html. Added some style to the body and container element in style.css. Added functionalities for the grid container and the resize button in script.js

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let clear = document.querySelector("button");
+let container = document.querySelector(".container");
+
+        
+//function to create a 16x16 grid
+function createGrid(size){
+
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size*size; i++) {
+        
+        let square = document.createElement("div");
+        square.style.backgroundColor = "black";
+        container.appendChild(square);
+
+ //change background color of a square on hover
+        square.addEventListener('mouseover', e=>{
+            square.style.backgroundColor = "white";
+        })
+
+        
+// adding Event LIsteners
+clear.addEventListener('click', ()=>{ 
+    resetSize()
+})
+
+
+//function to reset the grid square size
+function resetSize(){
+        
+    let number = prompt("What size would you like the grid to be? (1-100)");
+    container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    createGrid(number); 
+}
+
+
+//function to reset the grid
+        function clearGrid(){
+            clear.addEventListener('click', e=>{
+                square.style.backgroundColor = "black"
+           })
+        }
+        
+
+        clearGrid();    
+    }
+}
+
+createGrid(16);
